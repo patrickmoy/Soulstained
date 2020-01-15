6e3d0bffeb35b9254e6281b787a841318f4aa9e7
SteTR <43940276+SteTR@users.noreply.github.com>
2020-01-15 09:52:20 -0800

    Updated files to test the game engine and game engine related components

---
+++ js/mainTest.js
@@ -0,0 +1,29 @@
+let testManager = new ImageManager();
+
+testManager.queueDownload("./res/img/testImage.png");
+testManager.queueDownload('./res/img/RobotUnicorn.png');
+testManager.queueDownload("./res/img/testLargeImage.jpg");
+testManager.queueDownload("./res/img/testLargeImage2.jpg");
+testManager.startDownload().then(() =>
+{
+  var canvas = document.getElementById("gameWorld");
+  var ctx = canvas.getContext("2d");
+
+  var img = testManager.getImage("./res/img/testImage.png");
+
+  ctx.drawImage(img, 100, 100);
+
+  console.log('Finished drawing!');
+});
+
+// testManager.startDownload(() =>
+// {
+//   var canvas = document.getElementById("gameWorld");
+//   var ctx = canvas.getContext("2d");
+//
+//   var img = testManager.getImage("./res/img/testImage.png");
+//
+//   ctx.drawImage(img, 100, 100);
+//
+//   console.log('Finished drawing!');
+// });
