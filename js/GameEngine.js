b97797b90d84beb26dafbf33dd598381fa2ef370
SteTR <43940276+SteTR@users.noreply.github.com>
2020-01-15 20:43:51 -0800

    Game engine loop and key press register is functioning.

--- js/GameEngine.js
+++ js/GameEngine.js
@@ -3,21 +3,68 @@
   * Modified to work with our game.
   */
 
+  window.requestAnimFrame = (function () {
+      return window.requestAnimationFrame ||
+              window.webkitRequestAnimationFrame ||
+              window.mozRequestAnimationFrame ||
+              window.oRequestAnimationFrame ||
+              window.msRequestAnimationFrame ||
+              function (/* function */ callback, /* DOMElement */ element) {
+                  window.setTimeout(callback, 1000 / 60);
+              };
+  })();
+
 class GameEngine
 {
-  constructor()
+  constructor(ctx)
   {
-    this._inputs = [];
+    this.inputs = [];
+    this._context = ctx;
+    this.createInputs("Space");
+    this.startInput();
   }
 
+  update()
+  {
+    // Do something
+  }
+
+  loop()
+  {
+
+  }
+
+  run()
+  {
+    var self = this;
+    console.log("Game is starting...");
+    function gameLoop()
+    {
+      self.loop();
+      // Random note: interesting, when I did gameLoop(), it would cause a stack error
+      // However, when I just use gameLoop, it fixed it.
+      //I guess it's because you're literally calling function and causing an infinite loop?
+      requestAnimFrame(gameLoop, self._context.canvas);
+    }
+    gameLoop();
+  }
+
+  startInput()
+  {
+    var self = this;
+    this._context.canvas.addEventListener("keydown", (key) =>
+    {
+      if(self.inputs.hasOwnProperty(key.code))
+      {
+        console.log(key.code + " was hit!");
+        self.inputs[key.code] = true;
+      }
+    });
+  }
+
+  // Not sure if we should even keep this or change it.
   createInput(keyCode)
   {
-    console.log(keyCode);
-    this._inputs[keyCode] = false;
-  }
-
-  getInput(keyCode)
-  {
-    return this._inputs[keyCode];
+    this.inputs[keyCode] = false;
   }
 }
