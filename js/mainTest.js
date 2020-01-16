e14bb44f7c10b6d3bed63768baf07e7baf73502e
SteTR <43940276+SteTR@users.noreply.github.com>
2020-01-15 20:44:14 -0800

    Updated mainTest to test game engine loop and key press

--- js/mainTest.js
+++ js/mainTest.js
@@ -7,25 +7,6 @@ testManager.startDownload().then(() =>
 {
   var canvas = document.getElementById("gameWorld");
   var ctx = canvas.getContext("2d");
-
-  var img = testManager.getImage("./res/img/testImage.png");
-
-  ctx.drawImage(img, 100, 100);
-
-  console.log('Finished drawing!');
+  var gameEng = new GameEngine(ctx);
+  gameEng.run();
 });
-
-// testManager.startDownload(() =>
-// {
-//   var canvas = document.getElementById("gameWorld");
-//   var ctx = canvas.getContext("2d");
-//
-//   var img = testManager.getImage("./res/img/testImage.png");
-//
-//   ctx.drawImage(img, 100, 100);
-//
-//   console.log('Finished drawing!');
-// });
-
-var gameEng = new GameEngine();
-gameEng.createInput("space");
