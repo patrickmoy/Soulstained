bfda03d45d5fa30586787b77f453290f04941f5c
SteTR <43940276+SteTR@users.noreply.github.com>
2020-01-18 19:47:38 -0800

    camera animation is functioning for testing. Refactored some code for performance

--- js/Background.js
+++ js/Background.js
@@ -1,18 +1,81 @@
-class Background {
-  constructor(game, backgroundImage) {
-    this.x = 0;
-    this.y = 0;
-    this.spriteSheet = backgroundImage;
-    this.game = game;
-    this.ctx = this.game.ctx;
-  }
+class Background
+{
+	constructor(game, backgroundImage)
+	{
+    this.SOURCE_SHIFT_X = 2;
+    this.SOURCE_SHIFT_Y = 2;
+		this.sourceX = 0;
+		this.sourceY = 0;
+		this.sourceWidth = 180;
+		this.sourceHeight = 150;
+    this.MIN_X = 0;
+    this.MIN_Y = 0;
+    this.MAX_X = backgroundImage.width;
+    this.MAX_Y = backgroundImage.height;
+		this.spriteSheet = backgroundImage;
+		this.game = game;
+		this.ctx = this.game.ctx;
+	}
 
-  draw() {
-    this.ctx.drawImage(this.spriteSheet, 0, 0, 500, 224, this.x, this.y + 76, 1000, 448);
-  }
+	draw()
+	{
+		this.ctx.drawImage(this.spriteSheet, this.sourceX, this.sourceY,
+                       this.sourceWidth, this.sourceHeight, 0, 0,
+                       this.ctx.canvas.width, this.ctx.canvas.height - 150);
+	}
 
-  update() {
-    //Does nothing because the background does not change.
-  }
+	update()
+	{
+    // Can't update to next window with the prototype because of how the keys are polled
+		if (this.game.inputs["KeyW"])
+		{
+      if (this.sourceY >= this.MIN_Y)
+      {
+        this.sourceY -= this.SOURCE_SHIFT_Y;
+      }
+      else
+      {
+        console.log("Upper height bound reached");
+      }
+		}
+		if (this.game.inputs["KeyA"])
+		{
+      if (this.sourceX >= this.MIN_X)
+      {
+        this.sourceX -= this.SOURCE_SHIFT_X;
+      }
+      else
+      {
+        console.log("Left width bound reached");
+      }
 
+		}
+
+		if (this.game.inputs["KeyS"])
+		{
+      if (this.sourceY <= this.MAX_Y)
+      {
+        this.sourceY += this.SOURCE_SHIFT_Y;
+        console.log("X: " + this.sourceX + " Y: " + this.sourceY);
+      }
+      else
+      {
+        console.log("Lower height bound reached");
+      }
+		}
+
+		if (this.game.inputs["KeyD"])
+		{
+      if (this.sourceX <= this.MAX_X)
+      {
+        this.sourceX += this.SOURCE_SHIFT_X;
+        console.log("X: " + this.sourceX + " Y: " + this.sourceY);
+      }
+      else
+      {
+        console.log("Right width bound reached");
+      }
+
+		}
+	}
 }
--- js/mainTest.js
+++ js/mainTest.js
@@ -1,9 +1,8 @@
+
 let testManager = new ImageManager();
 
 testManager.queueDownload("./res/img/testImage.png");
-testManager.queueDownload("./res/img/testLargeImage.jpg");
-testManager.queueDownload("./res/img/testLargeImage2.jpg");
-testManager.queueDownload("./res/img/bridge.png");
+testManager.queueDownload("./res/img/testBackground.png");
 testManager.queueDownload("./res/img/simon.png");
 testManager.startDownload().then(() =>
 {
@@ -17,6 +16,6 @@ testManager.startDownload().then(() =>
 
   gameEng.init();
   gameEng.run();
-  gameEng.addEntity(new Background(gameEng, testManager.getImage("./res/img/bridge.png")));
+  gameEng.addEntity(new Background(gameEng, testManager.getImage("./res/img/testBackground.png")));
   gameEng.addEntity(new Hunter(gameEng, testManager.getImage("./res/img/simon.png")));
 });
