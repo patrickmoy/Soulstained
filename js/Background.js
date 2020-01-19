86b373e9dc70f5c9ec21f27bf9f9fb63783205b3
SteTR <43940276+SteTR@users.noreply.github.com>
2020-01-19 00:22:40 -0800

    Cleaned background

--- js/Background.js
+++ js/Background.js
@@ -2,16 +2,15 @@ class Background
 {
 	constructor(game, backgroundImage)
 	{
-    this.SOURCE_SHIFT_X = 2;
-    this.SOURCE_SHIFT_Y = 2;
+    this.SOURCE_SHIFT = 5;
 		this.sourceX = 0;
 		this.sourceY = 0;
-		this.sourceWidth = 180;
-		this.sourceHeight = 150;
+		this.sourceWidth = 192;
+		this.sourceHeight = 192;
     this.MIN_X = 0;
     this.MIN_Y = 0;
-    this.MAX_X = backgroundImage.width;
-    this.MAX_Y = backgroundImage.height;
+    this.MAX_X = backgroundImage.width - this.sourceWidth - 3;
+    this.MAX_Y = backgroundImage.height - this.sourceHeight - 3;
 		this.spriteSheet = backgroundImage;
 		this.game = game;
 		this.ctx = this.game.ctx;
@@ -29,9 +28,9 @@ class Background
     // Can't update to next window with the prototype because of how the keys are polled
 		if (this.game.inputs["KeyW"])
 		{
-      if (this.sourceY >= this.MIN_Y)
+      if (this.sourceY > this.MIN_Y)
       {
-        this.sourceY -= this.SOURCE_SHIFT_Y;
+        this.sourceY -= this.SOURCE_SHIFT;
       }
       else
       {
@@ -40,40 +39,43 @@ class Background
 		}
 		if (this.game.inputs["KeyA"])
 		{
-      if (this.sourceX >= this.MIN_X)
+      if (this.sourceX > this.MIN_X)
       {
-        this.sourceX -= this.SOURCE_SHIFT_X;
+        this.sourceX -= this.SOURCE_SHIFT;
       }
       else
       {
         console.log("Left width bound reached");
+				console.log("X: " + this.sourceX + " Y: " + this.sourceY);
       }
 
 		}
 
 		if (this.game.inputs["KeyS"])
 		{
-      if (this.sourceY <= this.MAX_Y)
+      if (this.sourceY < this.MAX_Y)
       {
-        this.sourceY += this.SOURCE_SHIFT_Y;
+        this.sourceY += this.SOURCE_SHIFT;
         console.log("X: " + this.sourceX + " Y: " + this.sourceY);
       }
       else
       {
         console.log("Lower height bound reached");
+				console.log("X: " + this.sourceX + " Y: " + this.sourceY);
       }
 		}
 
 		if (this.game.inputs["KeyD"])
 		{
-      if (this.sourceX <= this.MAX_X)
+      if (this.sourceX < this.MAX_X)
       {
-        this.sourceX += this.SOURCE_SHIFT_X;
+        this.sourceX += this.SOURCE_SHIFT;
         console.log("X: " + this.sourceX + " Y: " + this.sourceY);
       }
       else
       {
         console.log("Right width bound reached");
+				console.log("X: " + this.sourceX + " Y: " + this.sourceY);
       }
 
 		}
