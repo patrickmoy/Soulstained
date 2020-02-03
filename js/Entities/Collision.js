e1955e2e97290f778a6808527096136289035111
Patrick Moy <patrickrnmoy@gmail.com>
2020-02-03 14:54:50 -0800

    Added Collision.js to js/Entities

---
+++ js/Entities/Collision.js
@@ -0,0 +1,26 @@
+class Collision {
+  constructor() {}
+
+  detectCollide(entitiesToCheck) {
+    var i, j, firstElement, secondElement;
+    var entityCount = entitiesToCheck.length;
+    var currentCollisionPairs = [];
+
+    // Dead entities don't need to collide
+    // DECISION: DO we check if an entity necessitates checking here or elsewhere?
+    for (i = 0; i < entityCount; i++) {
+      firstElement = entitiesToCheck[i];
+
+      for (j = i + 1; j < entityCount; j++) {
+        secondElement = entitiesToCheck[j];
+
+        if (this.entitiesCollided(firstElement, secondElement)) {
+          currentCollisionPairs.push([firstElement, secondElement]);
+        }
+      }
+    }
+    return currentCollisionPairs;
+  }
+
+
+}
