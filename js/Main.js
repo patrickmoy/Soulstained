e7ac5469ec724e568a1a9ce609c6ac5a9e8c2ead
Steven <43940276+SteTR@users.noreply.github.com>
2020-02-23 21:12:59 -0800

    changed test message

--- js/Main.js
+++ js/Main.js
@@ -1,4 +1,4 @@
-console.log("Code has started running")
+console.log("Code has started running2");
 const AssetHandler = new AssetManager();
 AssetHandler.queueImage('./res/img/worlds/testBossRoom.png');
 AssetHandler.queueImage('./res/img/worlds/testBossRoom2.png');
