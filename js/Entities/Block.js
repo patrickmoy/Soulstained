de61503c4bcdb9cabbe52efa85fe66ab0efce20d
Steven <43940276+SteTR@users.noreply.github.com>
2020-03-15 21:46:06 -0700

    added collision logs with wolf

--- js/Entities/Block.js
+++ js/Entities/Block.js
@@ -257,7 +257,8 @@ class RollingPin extends Block {
     preUpdate() {
         super.preUpdate();
         this.pushUpdate = true;
-        if (detectCollide([this], this.game.currentEntities[1]).length >= 2) {
+        console.log(detectCollide([this], this.game.currentEntities[2]));
+        if (detectCollide([this], this.game.currentEntities[1]).length >= 2 || detectCollide([this], this.game.currentEntities[2]).length >= 2) {
             switch (this.direction) {
                 case "SOUTH":
                     this.direction = "NORTH";
